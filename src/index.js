09ba7b15d55122a0ac155a327169dd42a973c24e
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-22 13:42:32 -0500

    added a body to the page and a footer. to start the general layout

--- src/index.js
+++ src/index.js
@@ -6,4 +6,14 @@ const header = document.createElement('h1');
 header.textContent = 'Project Manager';
 header.classList.add('projectHeader');
 
+const footer = document.createElement('div');
+footer.classList.add('projectFooter');
+
+const pageView = document.createElement('div');
+pageView.classList.add('pageView');
+
+
+
 content.appendChild(header);
+content.appendChild(pageView);
+content.appendChild(footer);
