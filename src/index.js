68a3c2b6065c7a7482b817d108cdb284be61d822
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-22 13:57:03 -0500

    cleaned up the footer link

--- src/index.js
+++ src/index.js
@@ -6,11 +6,21 @@ const header = document.createElement('h1');
 header.textContent = 'Project Manager';
 header.classList.add('projectHeader');
 
-const footer = document.createElement('div');
-footer.classList.add('projectFooter');
 
 const pageView = document.createElement('div');
 pageView.classList.add('pageView');
+
+const footer = document.createElement('div');
+footer.classList.add('projectFooter');
+
+const date = new Date().getFullYear();
+footer.innerHTML = `<a href="https://github.com/SalamanderCtesiphon/resturant-page-project" class="footer-link">
+<h5>Seamus Quixote  &#169;  <a href="https://www.theodinproject.com/" class="footer-link"> The Odin Project</a>  <span id="currentYear">${date}</span> </h5>
+</a>`;
+footer.classList.add('footer-link');
+
+
+
 
 
 
